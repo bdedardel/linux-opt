3595728aefa3cca34775be48461eab2bfc1151b1
bdedardel <bdedardel@numeriware.com>
2018-07-14 09:50:56 -1000

    apt-get-init-srv.sh

--- Readme.md
+++ Readme.md
@@ -5,4 +5,5 @@ Some linux opt custom scripts
 
 - bin/apt-get-install.sh: install debian package and commit files
 - bin/apt-get-upgrade.sh: update/upgrade debian packages and commit files
+- bin/bin/apt-get-init-srv.sh: install some srv packages
 - bin/npm-install.sh: : install npm package and commit files
---
+++ bin/apt-get-init-srv.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# admin
+# apt-get install cron-apt
+apt-get install debian-goodies
+apt-get install apt-file
+apt-get install npm
+
+apt-get install alien
+apt-get install lshw
+apt-get install lshw-gtk
+# apt-get install wine
+# apt-get install kvm
+# apt-get -t wheezy-backports install qemu-system-x86
+
+apt-get install autofs
+apt-get install udftools
+apt-get install cryptsetup
+apt-get install jmtpfs
+
+apt-get clamav
+apt-get clamav-daemon
+apt-get install rkhunter
+apt-get install lynis
+apt-get install debsums
